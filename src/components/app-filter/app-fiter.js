888682cc1f4c65e87a94687b74922f8c50e3748a
ArtemZEL <artemzelenok60@gmail.com>
2022-08-11 02:23:41 +0300

    Фильтр и поиск

--- src/components/app-filter/app-fiter.js
+++ src/components/app-filter/app-fiter.js
@@ -1,24 +1,27 @@
 import './app-fiter.css';
 
 
-const AppFilter=()=>{
+const AppFilter=(props)=>{
+    const buttonsData=[
+        {  name:'all',label:'Все сотрудники'},        
+        { name:'rise',label:'Повышение'},        
+        { name:'moreThen1000', label:' З/п больше 1000$'},
+    ];
+    const buttons =buttonsData.map(({name,label})=>{
+        const active=props.filter===name;
+        const clazz=active ? 'btn-light':'btn-outline-light';
+        return (
+            <button type="button" 
+            className={`btn ${clazz}`}
+            key={name}
+            onClick={()=>props.onFilterSelect(name)}>
+                {label}
+            </button>
+        )
+    })
     return(
         <div className="btn-group">
-            <button type="button" 
-            className="btn btn-light"
-            >
-                Все сотдудники
-            </button>
-            <button type="button" 
-            className="btn btn-outline-light"
-            >
-                Повышение
-            </button>
-            <button type="button" 
-            className="btn btn-outline-light"
-            >
-                З/п больше 1000$
-            </button>
+            {buttons}
         </div>
     )
 }
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -15,7 +15,9 @@ class App extends Component{
                 {name: 'Artem Z.', salary: 900,increase:false,rise:true,id:1},
                 {name: 'Matveн G.', salary: 3000,increase:true,rise:false,id:2},
                 {name: 'Alexey Z.', salary: 5000,increase:false,rise:false,id:3}
-            ]
+            ],
+            term:'',
+            filter:'rise'
         }
         this.maxId=4;
     }
@@ -59,19 +61,50 @@ class App extends Component{
         }))
     }
 
+    searchEmp=(items,term)=>{
+        if(term.length===0)
+        {
+            return items;
+        }
+
+        return items.filter(item=>{
+            return item.name.indexOf(term)>-1
+        })
+    }
+    onUpdateSearch=(term)=>{
+        this.setState({term});
+    }
+    filterPost = (items,filter)=>{
+        switch(filter){
+            case 'rise':
+                return items.filter(item=>item.rise);
+            case 'moreThen1000':
+                return items.filter(item=>item.salary>1000);
+             default:
+                return items       
+        }
+    }
+
+    onFilterSelect = (filter)=>{
+        this.setState({filter});
+    }
+
     render(){
-       const employers=this.state.data.length;
-       const increase=this.state.data.filter(item=>item.increase).length;
+        const {data,term,filter}=this.state;
+        const employers=this.state.data.length;
+        const increase=this.state.data.filter(item=>item.increase).length;
+        const visibleData=this.filterPost(this.searchEmp(data,term),filter);
         return(
             <div className="app">
                 <AppInfo employers={employers} increase={increase}/>
                 <div className="search-panel">
-                    <SearchPanel/>
-                    <AppFilter/>
+                    <SearchPanel
+                    onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
      
                 </div>
                 <EmployersList 
-                data={this.state.data}
+                data={visibleData}
                 onDelete={this.deleteItem}
                 onToggleProp={this.onToggleProp}/>
                  <EmployersAddForm onAdd={this.addItem}/>
